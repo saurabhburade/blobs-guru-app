0120b8acf8203ac62d0883bd6d68f6346892e97c
saurabhburade <sburade3@gmail.com>
2024-10-10 15:13:52 +0530

    feat: add GA

--- src/app/(appRoutes)/layout.tsx
+++ src/app/(appRoutes)/layout.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 import { Providers } from "./providers";
 import { ThemeProvider } from "next-themes";
 import { Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Ethereum Blobs Explorer: Analyze L2 Transactions & EIP-4844 Data",
@@ -14,7 +15,7 @@ const space_grotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
-
+const GOOGLE_ANALYTICS_ID = "G-WZGPHNZWNN";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -38,6 +39,18 @@ export default function RootLayout({
         />
         <meta name="apple-mobile-web-app-title" content="Blobs Guru" />
         <link rel="manifest" href="/site.webmanifest" />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
       </head>
       <body className={space_grotesk.className}>
         <ThemeProvider>
