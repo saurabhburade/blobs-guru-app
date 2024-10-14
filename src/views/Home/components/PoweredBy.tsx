ee39ab19baea64ba78c818a510faa9fab2cc2e8b
saurabhburade <sburade3@gmail.com>
2024-10-15 00:47:45 +0530

    chore: typo

--- src/views/Home/components/PoweredBy.tsx
+++ src/views/Home/components/PoweredBy.tsx
@@ -6,7 +6,7 @@ type Props = {};
 function PoweredBy({}: Props) {
   return (
     <div className="py-5 flex gap-10  lg:h-[10em] justify-center flex-wrap ">
-      <div className="flex items-center gap-2  border-x lg:border-r lg:border-l-0 px-10 text-lg text-base-300">
+      <div className="flex items-center gap-2  border-x lg:border-r lg:border-l-0 border-base-200 px-10 text-lg text-base-300">
         <p className="font-bold"> 💪 Powered by</p>
       </div>
       <div className="flex items-center gap-2 ">
@@ -25,7 +25,7 @@ function PoweredBy({}: Props) {
       </div>
       <div className="flex items-center gap-2  ">
         <img src="/images/l2beat.png" width={50} height={50} alt="" />
-        <p className="font-bold">L2beat</p>
+        <p className="font-bold">L2BEAT</p>
       </div>
     </div>
   );
