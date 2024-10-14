59e66c06b5f76d03c5d977f6988d04dc14508feb
saurabhburade <sburade3@gmail.com>
2024-10-14 13:19:45 +0530

    fix: favicon

--- src/app/(appRoutes)/layout.tsx
+++ src/app/(appRoutes)/layout.tsx
@@ -30,10 +30,13 @@ export default function RootLayout({
           href="/favicon-48x48.png"
           sizes="48x48"
         />
-        <link rel="icon" href="/favicon.ico" />
 
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="https://blobs.guru/favicon.svg?v=2"
+        />
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
