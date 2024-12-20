acde2f50921c4399c4e65a785b82011fc8e85352
Saurabh Burade <sburade3@gmail.com>
2024-12-20 14:41:30 +0530

    chore: remove unused query

--- src/views/Avail/AvailSummary.tsx
+++ src/views/Avail/AvailSummary.tsx
@@ -4,9 +4,6 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 import React from "react";
 import PoweredBy from "../Home/components/PoweredBy";
 import Footer from "@/components/Footer/Footer";
-import { useQuery } from "@apollo/client";
-import { AVAIL_COLLECTIVE_STAT_QUERY } from "@/lib/apollo/queriesAvail";
-import { availClient } from "@/lib/apollo/client";
 import AvailStats from "./components/AvailStats";
 import AvailAccounts from "./components/AvailAccounts";
 
@@ -23,7 +20,7 @@ function AvailSummary({}: Props) {
       </div>
       <div className="p-5 min-h-[90vh] h-screen overflow-scroll flex flex-col space-y-4 pb-10 ">
         <div className=" w-full lg:flex-row flex-col flex justify-between gap-4 items-center lg:my-0 my-[5em]">
-        <img src="/images/avail_logo.png" alt="" className="w-[140px]" />
+          <img src="/images/avail_logo.png" alt="" className="w-[140px]" />
         </div>
         <div className="w-full space-y-4">
           <AvailStats />
