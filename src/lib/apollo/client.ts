042663c434e3a32a32756741c4c1a6c93ca1e076
Saurabh Burade <sburade3@gmail.com>
2025-01-05 14:52:26 +0530

    chore: refactor

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -2,10 +2,11 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const subgraphUrl =
   "https://api.studio.thegraph.com/query/90545/blobs-explorer/vt1.5(np)";
 const availUrl = "https://availsubquery2.blobs.guru/";
-const availUrl2 = "https://availsubquery2.blobs.guru/";
+// const availUrl2 = "https://availsubquery2.blobs.guru/";
 // const availUrl2 = "https://availsubquery.blobs.guru/";
 // const availUrl2 = "http://50.28.2.32:3000";
-// const availUrl2 = "https://availwd.blobs.guru/"; //"http://92.113.144.195:3000";
+// const availUrl2 = "http://103.106.59.167:3000";
+const availUrl2 = "https://availwd.blobs.guru/"; //"http://92.113.144.195:3000";
 // const availUrl2 = "http://69.167.167.199:3000";
 
 // const httpLink = new HttpLink({ uri: subgraphUrl });
