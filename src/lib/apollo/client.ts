5d4080ef05b9182402dc4fe7d142583350d343eb
saurabhburade <sburade3@gmail.com>
2025-06-30 16:01:37 +0530

    chore: update client api

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -2,11 +2,11 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const subgraphUrl =
   // "https://api.studio.thegraph.com/query/90545/blobs-explorer/vt1.5(np)";
   // "https://api.studio.thegraph.com/query/90545/blobs-explorer/vt1.6.24";
-  "http://103.50.32.155:8000/subgraphs/name/blobs-explorer";
+  "https://api.studio.thegraph.com/query/90545/blobs-explorer/vt1.6.26";
 // const availUrl = "https://wd.blobs.guru/";
 // const availUrl = "http://161.97.94.52:3000";
 const availUrl = "https://availapi.blobs.guru/";
-const celestiaUrl = "http://103.50.32.159:3000";
+const celestiaUrl = "https://celestiaapi.blobs.guru/";
 
 export const apolloClient = new ApolloClient({
   uri: subgraphUrl,
