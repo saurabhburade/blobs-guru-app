538c18c56ad6f1b38ebbc930bb23698995541f51
saurabhburade <sburade3@gmail.com>
2026-03-02 18:26:38 +0530

    build: refresh lockfiles and fix build cache issues

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const subgraphUrl =
   // "https://api.studio.thegraph.com/query/90545/blobs-explorer/vt1.5(np)";
   // "https://api.studio.thegraph.com/query/90545/blobs-explorer/vt1.6.24";
-  "https://ethapi.blobs.guru";
+  "https://ethapi.blobs.guru/";
 // const availUrl = "https://wd.blobs.guru/";
 // const availUrl = "http://161.97.94.52:3000";
 const availUrl = "https://availapi.blobs.guru/";
